2dd294dbb95ad375208fded81d740ef012c3913b
Oscar Matias Gonzalez <59715871+oscarmatiasg@users.noreply.github.com>
2025-06-10 19:59:32 -0300

    edit-login

--- frontend/src/components/Navb.jsx
+++ frontend/src/components/Navb.jsx
@@ -4,6 +4,7 @@ import { NavLink, useNavigate } from 'react-router-dom'
 const Navb = () => {
 
     const navigate = useNavigate();
+    
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-gray-400'>
@@ -26,8 +27,12 @@ const Navb = () => {
                 <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
         </ul>
-        <div>
-            <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-md font-medium hover:bg-primary/80 transition-all duration-400 mr-3'>
+        <div className='flex items-center gap-4'>
+            <div>
+                <img src={assets.profile_pic} alt="" />
+                <img src={assets.dropdown_icon} alt="" />
+            </div>
+            :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-md font-medium hover:bg-primary/80 transition-all duration-400 mr-3'>
                 Crear cuenta
             </button>
         </div>
