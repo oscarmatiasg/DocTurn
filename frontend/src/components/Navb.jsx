77a642d0c0feaefab4b90085c93ec0708517d873
Oscar Matias Gonzalez <59715871+oscarmatiasg@users.noreply.github.com>
2025-06-11 17:04:48 -0400

    Navbar_edit

--- frontend/src/components/Navb.jsx
+++ frontend/src/components/Navb.jsx
@@ -5,6 +5,8 @@ const Navb = () => {
 
     const navigate = useNavigate();
     
+    const [showMenu, setShowMenu] = React.useState(false)   
+    const [token, setToken] = React.useState(true)
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-gray-400'>
@@ -28,13 +30,21 @@ const Navb = () => {
             </NavLink>
         </ul>
         <div className='flex items-center gap-4'>
-            <div>
-                <img src={assets.profile_pic} alt="" />
-                <img src={assets.dropdown_icon} alt="" />
-            </div>
-            :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-md font-medium hover:bg-primary/80 transition-all duration-400 mr-3'>
-                Crear cuenta
-            </button>
+            {
+                token ?
+                <div className='flex items-center gap-2 cursor-pointer relative' onClick={()=>setShowMenu(!showMenu)}>
+                    <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+                    <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+                    <div className='absolute top-10 right-0 bg-white shadow-lg rounded-md p-3 z-10 text-black font-medium flex flex-col gap-2' style={{display: showMenu ? 'block' : 'none'}}>
+                        <div className='min-w-48 bg-stone-100 rounded p-4 text-black flex flex-col gap-4'>
+                            <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-ponter'>Mi perfil</p>
+                            <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-ponter'>Mis citas</p>
+                            <p onClick={()=>setToken(false)} className='hover:text-black cursor-ponter'>Cerrar sesión</p>
+                        </div>
+                    </div>
+                </div>
+                :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-md font-medium hover:bg-primary/80 transition-all duration-400 mr-3'>Iniciar Sesión</button>
+            }
         </div>
     </div>
   )
